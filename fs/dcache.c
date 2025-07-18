f305437fd05fe9630a4b8971d08b9f94c360290f
xfwdrev <74693315+xfwdrev@users.noreply.github.com>
2025-07-19 03:52:47 +0800

    kernel: Move all SUS_ Flag checks from inode->i_state to inode->i_mapping->flags
    - All sus features that rely on inode->i_state is now moved to inode->i_mapping->flags it is more reliable since inode->i_state can be somehow reset even the stat data is not changed while i_mapping->flags is not.
    
    Co-authored-by: simonpunk <simonpunk2016@gmail.com>

--- fs/dcache.c
+++ fs/dcache.c
@@ -2299,7 +2299,7 @@ seqretry:
 			if (dentry_cmp(dentry, str, hashlen_len(hashlen)) != 0)
 				continue;
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-			if (dentry->d_inode && unlikely(dentry->d_inode->i_state & INODE_STATE_SUS_PATH) && likely(susfs_is_current_non_root_user_app_proc())) {
+			if (dentry->d_inode && unlikely(dentry->d_inode->i_mapping->flags & BIT_SUS_PATH) && likely(susfs_is_current_non_root_user_app_proc())) {
 				continue;
 			}
 #endif
@@ -2387,7 +2387,7 @@ struct dentry *__d_lookup(const struct dentry *parent, const struct qstr *name)
 			continue;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-		if (dentry->d_inode && unlikely(dentry->d_inode->i_state & INODE_STATE_SUS_PATH) && likely(susfs_is_current_non_root_user_app_proc())) {
+		if (dentry->d_inode && unlikely(dentry->d_inode->i_mapping->flags & BIT_SUS_PATH) && likely(susfs_is_current_non_root_user_app_proc())) {
 			continue;
 		}
 #endif
